543aa89f1ed4fe7966b32ae2e6c55c5cf0ad9e74
BuildTools <unconfigured@null.spigotmc.org>
2019-08-09 01:54:46 +0300

    + Database fix

--- js/ContainerLoader.js
+++ js/ContainerLoader.js
@@ -1,9 +1,9 @@
 function PreLoadContainers() {
     $.ajax({
-        url: `http://justkato.me/Database/containers.json`,
+        url: `https://raw.githubusercontent.com/KatoTwofold/KatoTwofold.github.io/master/Database/containers.json`,
         type: `get`,
         complete: (msg) => {
-            if ( msg.status == 200 ) {
+            if (msg.status == 200) {
                 LoadContainers(JSON.parse(msg.responseText));
             } else {
                 console.error("There has been an error loading the database, disabling the website.")
@@ -13,5 +13,21 @@ function PreLoadContainers() {
 }
 
 function LoadContainers(container_data) {
-    console.log(container_data);
+    let about = container_data.about;
+    let experience = container_data.experience;
+    let projects = container_data.projects;
+
+    for (let i = 0; i < projects.length; i++) {
+        let html =
+            `
+            <div class="section">
+                <div class="bg-img" style="background-image: url('${projects[i]["bg-image"]}')"></div>
+                <div class="bg-gradient"></div>
+                <div class="section-title">${projects[i].title}</div>
+                <div class="description">${projects[i].description}</div>
+            </div>
+            `;
+        $('.container[category="projects"]').append(html);
+    }
+
 }
