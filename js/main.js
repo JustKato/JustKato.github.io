065b700767a279ea570063d0866cff289d2c66ce
BuildTools <unconfigured@null.spigotmc.org>
2019-08-08 07:30:53 +0300

    + Tested jquery auto-complete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(document).ready(() => {
     console.log("Successfully Loaded the document");
 });
+
+$('.main-wrapper').on("click", (event) => {
+    console.log("Clicked the main wrapper!");
+})
