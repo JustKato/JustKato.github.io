4f68eafdf6c28773ef115f995f95bd8d755f2121
BuildTools <unconfigured@null.spigotmc.org>
2019-08-10 02:20:32 +0300

    * Updated the database

--- js/ContainerLoader.js
+++ js/ContainerLoader.js
@@ -13,16 +13,16 @@ function PreLoadContainers() {
 }
 
 function LoadContainers(container_data) {
-    let experience = container_data.experience;
+    let abilities = container_data.abilities;
     let projects = container_data.projects;
 
-    for (let i = 0; i < experience.length; i++) {
+    for (let i = 0; i < abilities.length; i++) {
         let html =
             `
             <div class="section">
-                <div class="section-title">${experience[i].title}</div>
-                <div class="description">${experience[i].description}</div>
-                <div class="bg-img" style="background-image: url('${experience[i]["bg-image"]}')"></div>
+                <div class="section-title">${abilities[i].title}</div>
+                <div class="description">${abilities[i].description}</div>
+                <div class="bg-img" style="background-image: url('${abilities[i]["bg-image"]}')"></div>
                 <div class="bg-gradient"></div>
             </div>
             `;
